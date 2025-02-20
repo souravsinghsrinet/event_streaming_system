ae1884a6b4386cdafe1fbcd4bddbe17eecbb40ab
Sourav Singh <souravsingh@Souravs-MacBook-Pro.local>
2025-02-20 21:28:56 +0530

    Added events endpoint which is up and running

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from src.app.routes.v1 import track_event_router
 from threading import Thread
 from src.app.utils.kafka_helper import KafkaHelper
+from src.app.utils.db_helper import create_tables
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🔧 Creating tables before the app starts...")
+    create_tables()
+    print("✅ Tables verified successfully!")
+    yield  # Continue running the app
 
 def start_consumer():
     k_h_obj = KafkaHelper()
@@ -12,7 +21,12 @@ def start_consumer():
 consumer_thread = Thread(target=start_consumer, daemon=True)
 consumer_thread.start()
 
-app = FastAPI(debug=True)
+app = FastAPI(lifespan=lifespan)
+# app = FastAPI(debug=True)
+
+# @app.on_event("startup")
+# def startup_event():
+#     create_tables()
 
 app.include_router(track_event_router)
 
