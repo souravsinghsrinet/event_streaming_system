c78a276036c87fc1d0938396eed9aaa826a234e1
Sourav Singh <souravsingh@Souravs-MacBook-Pro.local>
2025-03-13 15:50:03 +0530

    Added cache to store frequently accessed data

--- src/app/services/event_service.py
+++ src/app/services/event_service.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
 from src.app.models.event_model import Event
+from src.app.utils.cache_helper import (
+    get_cache_events, set_cached_events
+)
 
 class EventService:
 
@@ -15,9 +18,15 @@ class EventService:
         self.db.refresh(new_event)
 
     def get_event_data(self, event_name: str = None, limit: int = 10, offset: int = 0):
+        cache_key = f"events:{event_name}:{limit}:{offset}"
+        cached_data = get_cache_events(key=cache_key)
+        if cached_data:
+            return cached_data
         query = self.db.query(Event)
         if event_name:
             query = query.filter(Event.event_name == event_name)
         events = query.offset(offset).limit(limit).all()
-        return events
+        resp_data = [{"event_name": event.event_name, "user_id": event.user_id} for event in events]
+        set_cached_events(key=cache_key, data=resp_data)
+        return resp_data
 
--- src/app/utils/constants.py
+++ src/app/utils/constants.py
@@ -1,4 +1,15 @@
 import os
 
+# DB config
 DB_URL = os.environ.get("DB_URL")
+
+# Kafka config
 KAFKA_BROKER = os.environ.get("KAFKA_BROKER")
+
+# Redis config
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
+CACHE_EXPIRY = 60  # Cache expiry in seconds
+
+# Store active WebSocket connections
+ACTIVE_CONNECTIONS = set()
